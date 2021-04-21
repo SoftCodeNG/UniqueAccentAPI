76f7f326f818741866dc4856cab91a157f9c2b22
Augustine Nwafor <austineforall@gmail.com>
2021-04-21 09:23:46 +0100

    Initial Commit

--- UniqueAccentApi/settings.py
+++ UniqueAccentApi/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/rest_framework/')
+STATIC_URL = '/static/rest_framework/'
 
 # Media Root
 MEDIA_ROOT = os.path.join(BASE_DIR, 'UploadedMediaFiles')
