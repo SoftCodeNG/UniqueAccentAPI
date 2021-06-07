b948495e71f55270dcb79031179b6032e4d4bb12
Augustine Nwafor <austineforall@gmail.com>
2021-06-07 01:07:14 +0100

    Worked on Get Course Lessons

--- UniqueAccentApi/settings.py
+++ UniqueAccentApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env['DEBUG']
 
-ALLOWED_HOSTS = ['unique-accent-api.herokuapp.com', 'localhost:4200', env['ALLOWED_HOSTS']]
+ALLOWED_HOSTS = ['unique-accent-api.herokuapp.com', 'localhost:4200', 'localhost:5200', env['ALLOWED_HOSTS']]
 
 
 # Application definition
