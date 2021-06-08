e9c573cdaa251a49f5adf47c53aed35fa3d8fbf8
Augustine Nwafor <austineforall@gmail.com>
2021-06-08 09:38:57 +0100

    Worked on Get Course Lessons

--- UniqueAccentApi/settings.py
+++ UniqueAccentApi/settings.py
@@ -152,7 +152,7 @@ MEDIA_URL = '/get-media/'
 # SimpleJWT Configuration
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
--- courses/serializer.py
+++ courses/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
+from accounts.models import UserAccount
 from courses.models import Courses, Lessons, Comments, Replies
 
 
@@ -52,17 +53,22 @@ class PostCommentSerializer(ModelSerializer):
 
 
 class ReplyCommentSerializer(ModelSerializer):
-    comment = serializers.CharField(required=False)
-
     class Meta:
         model = Replies
         fields = '__all__'
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = '__all__'
+
+
 class GetCommentSerializer(ModelSerializer):
     replies = ReplyCommentSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True, source='userId')
 
     class Meta:
         model = Comments
-        fields = ['lessonId', 'userId', 'comment', 'createdAt', 'updatedAt', 'replies']
+        fields = ['lessonId', 'user', 'comment', 'createdAt', 'updatedAt', 'replies']
 
